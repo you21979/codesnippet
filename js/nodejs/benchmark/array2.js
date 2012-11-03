1a96a84a8be2766524a02cc7862f5b67d4719de8
you21979 <you2197901@gmail.com>
2012-11-03 22:45:39 +0900

    配列をキューにしたときの速度チェック

---
+++ js/nodejs/benchmark/array2.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+[
+function(MAX){
+    var q = [];
+    var TID = "queue push:"+MAX;
+    console.time(TID);
+    for(var i = 0; i<MAX; ++i){
+        q.push(i);
+    }
+    console.timeEnd(TID);
+    var TID = "queue pop:"+MAX;
+    console.time(TID);
+    for(var i = 0; i<MAX; ++i){
+        var n = q.shift(i);
+        assert(n === i);
+    }
+    console.timeEnd(TID);
+},
+function(){
+}
+].forEach(function(fnc){
+    [1, 100, 1000, 10000, 100000].forEach(function(MAX){
+        fnc(MAX);
+    });
+    // 一回目と2回目で速度が大幅に違うのはフラグメントが関係してる？
+    [1, 100, 1000, 10000, 100000].forEach(function(MAX){
+        fnc(MAX);
+    });
+});
+
