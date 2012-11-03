a7ac86650feed4ae88570246cac820ac0c8af2b8
you21979 <you2197901@gmail.com>
2012-11-04 03:24:25 +0900

    pidをつけた

--- js/nodejs/npm/qilin_forever/app.js
+++ js/nodejs/npm/qilin_forever/app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('uoraoraoraora');
+  res.end(process.pid+':uoraoraoraora');
+  console.log(process.pid);
 }).listen(1337, '0.0.0.0');
