8f63ccb025a05b809974cd2a8e3e28753092a6e1
you21979 <you2197901@gmail.com>
2012-11-26 15:52:38 +0900

    mod try catch

--- js/newFunction/filevm.js
+++ js/newFunction/filevm.js
@@ -7,13 +7,20 @@ var MAX = 1000000;
     var ctx = {
         hoge : 0,
         fuga : 0,
+        Func : function(){
+            console.log("test");
+        },
     };
     var f = new Function(txt);
     var x = f();
     console.time(TID);
-    x.init(ctx);
-    for(var i=0;i<MAX;++i){
-        x.update(ctx);
+    try{
+        x.init(ctx);
+        for(var i=0;i<MAX;++i){
+            x.update(ctx);
+        }
+    }catch(e){
+        console.log(e.stack);
     }
     console.timeEnd(TID);
     console.log(ctx.hoge);
