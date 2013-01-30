0aa6411d748ce8bfcb0e246d818399bd4099698e
you21979 <you2197901@gmail.com>
2013-01-30 16:05:31 +0900

    mod

--- ts/0_hello/hello.ts
+++ ts/0_hello/hello.ts
@@ -1,10 +1,23 @@
 class Hello {
- constructor(public message: string) {}
- hello() {
-  return this.message;
- }
+    /**
+     * コンストラクタ
+     */
+    constructor(public message: string) {}
+    /**
+     * ヘロー
+     */
+    hello():string {
+        return this.message;
+    }
 };
-                        
-var h = new Hello("Hello, world!");
-var str = h.hello();
-console.log(str);
+
+class Main {
+    main(){
+        var h:Hello = new Hello("Hello, world!");
+        var str:string = h.hello();
+        console.log(str);
+    }
+};
+
+var m:Main = new Main();
+m.main();
